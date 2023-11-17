d9d67de9e4f47d3428e8841fac034a829e53062d
kingdapster <sal.carrazco89@gmail.com>
2023-11-17 15:24:37 -0800

    i found it i actually didnt have enough curlly braces

--- src/comps/Authenticate.jsx
+++ src/comps/Authenticate.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
 
-const Authenticate = ({token}) => {
-   const [error, setError] = useState(null);
-    async function handleClick(){
-      try{
+const Authenticate = ({ token }) => {
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
 
-      }catch(error){
-      setError(error.message)
-      
-        console.log(`it fires off`)
+    async function handleClick() {
+        try {
+            const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/authenticate`, {
+                method: `GET`, headers: {
+                    "Content-type": "application/json", Authorization: `Bearer ${token}`,
+                }
+            });
+        } catch (error) {
+            setError(error.message)
+            console.log(`it fires off`)
+        }}
+
+
+        return (
+            <>
+
+                <h2>Authenticate!</h2>
+                {error && <p>{error}</p>}
+                <button onClick={handleClick}>Authenticate</button>
+            </>
+        )
     }
 
-
-    return (
-    <>
-    
-        <h2>Authenticate!</h2>
-        {error && <p>{error}</p>}
-        <button onClick={handleClick}>Authenticate</button>
-    </>
-)
-}
-
-export default Authenticate;
+    export default Authenticate;
